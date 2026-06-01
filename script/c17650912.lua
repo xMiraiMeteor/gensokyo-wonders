b9aed904f21cc68a5d4330ceab0f0e06346c550e
Mirai Meteor <xmiraimeteor@gmail.com>
2026-06-01 23:53:56 +0800

    Update c17650912.lua

--- script/c17650912.lua
+++ script/c17650912.lua
@@ -57,7 +57,7 @@ function s.spcon(e,tp,eg,ep,ev,re,r,rp)
 	return c:IsPreviousLocation(LOCATION_MZONE) and c:IsXyzSummoned()
 end
 function s.spfilter(c,e,tp)
-	return c:IsSetCard({0x382,0x31e}) and not c:IsCode(id) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return c:IsSetCard({0x382,0x31e}) and c:IsLevelBelow(4) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.spfilter(chkc,e,tp) end
