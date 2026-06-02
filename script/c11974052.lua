347ccf7a706eaa6ed50abbf65cd8fe80f41c0013
Mirai Meteor <xmiraimeteor@gmail.com>
2026-06-02 13:06:12 +0800

    Update c11974052.lua

--- script/c11974052.lua
+++ script/c11974052.lua
@@ -25,6 +25,7 @@ function s.initial_effect(c)
 	e2:SetOperation(s.thop)
 	c:RegisterEffect(e2) 
 end
+s.listed_series={0x382,0x31e}
 function s.costfilter(c)
 	return c:IsSetCard(0x31e) and c:IsMonster() and c:IsAbleToGraveAsCost()
 end
